2438f120a5a8f631c3d250621c9fef80e1e40cf9
ab <ab@Alison.local>
2015-12-03 10:53:56 +0100

    first run

--- main.py
+++ main.py
@@ -12,36 +12,36 @@ IMAGE_SIZE = (600, 800)
 RESIZE_IMG = 0.5
 EDGE_THRES = 0.9
 
+#-------------------------------------------------------------------------------
 
-def get_integral_image(img):
-    #res = np.cumsum(img)
-    #res.reshape(img.shape)
+class IntegralImage:
+    def __init__(self, img):
+        self.int_img = np.zeros(img.shape, dtype='int')
     
-    res = np.zeros(img.shape, dtype='int')
+        for y in range(img.shape[0]):
+            zeile = 0
+            for x in range(img.shape[1]):
+                zeile += img[y,x]
+                self.int_img[y,x] = self.int_img[y-1,x] + zeile 
+    
+    
+    def sum(self, p1, p2):
+        """
+        to get sum from integral image, p1 and p2 are both included 
+            i.e. (0,0)(0,0) is value of first pixel
+                 (0,0)(1,1) is value of first 2x2 box
+                 (1,1)(1,1) is value of first diagonal pixel
+        """
+        s = self.int_img[p2]
+        if p1[1]>0:
+            s -= self.int_img[p2[0],p1[1]-1]
+        if p1[0]>0:
+            s -= self.int_img[p1[0]-1,p2[1]]
+        if p1[0]>0 and p1[1]>0:
+            s += self.int_img[p1[0]-1,p1[1]-1]
+        return s;
 
-    for y in range(img.shape[0]):
-        zeile = 0
-        for x in range(img.shape[1]):
-            zeile += img[y,x]
-            res[y,x] = res[y-1,x] + zeile 
-    return res
-
-
-def get_sum(int_img, p1, p2):
-    """
-    to get sum from integral image, p1 and p2 are both included 
-        i.e. (0,0)(0,0) is value of first pixel
-             (0,0)(1,1) is value of first 2x2 box
-             (1,1)(1,1) is value of first diagonal pixel
-    """
-    s = int_img[p2]
-    if p1[1]>0:
-        s -= int_img[p2[0],p1[1]-1]
-    if p1[0]>0:
-        s -= int_img[p1[0]-1,p2[1]]
-    if p1[0]>0 and p1[1]>0:
-        s += int_img[p1[0]-1,p1[1]-1]
-    return s;
+#-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     print "Lacunarity"
@@ -63,19 +63,21 @@ if __name__ == "__main__":
            [0,1,0,1,1,1,0,1,1,0,1,0],
            [0,1,0,0,0,1,0,1,1,1,0,1]])
 
-    int_img = get_integral_image(img)
+    int_img = IntegralImage(img)
 
-    print img
-    print int_img
-    print get_sum(int_img, (0,0),(0,0))
-    #print get_sum(int_img, (0,0),(11,11))
-    #print get_sum(int_img, (1,0),(11,11))
-    #print get_sum(int_img, (2,7),(8,9))
-    #print get_sum(int_img, (8,1),(11,5))
-    #print get_sum(int_img, (0,0),(1,1))
+
+    box_size = 2
+    counts = np.zeros(box_size * box_size+1)
     
+    #print int_img.sum((0,0),(1,1))
 
+    for y in range(img.shape[0]-1):
+        for x in range(img.shape[1]-1):
+            s = int_img.sum((y,x),(y+box_size-1,x+box_size-1))
+            counts[s] += 1
 
+    print counts
+ 
 
     # plt.figure(4),plt.imshow(distribution, cmap="jet"), plt.colorbar()
     # plt.show()
